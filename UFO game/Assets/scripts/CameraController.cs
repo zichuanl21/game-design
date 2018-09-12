e5bd28e107c3ca58eebdea8db07187d049b69d6d
zichuanl21 <zichuanl21@berkeley.edu>
2018-09-11 17:07:23 -0700

    lolololololol

---
+++ UFO game/Assets/scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject p;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - p.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = offset + p.transform.position;
+	}
+}
---
+++ UFO game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    public Text ctext;
+
+    public Text wtext;
+
+    private int c;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        c = 0;
+        wtext.text = "";
+        setCount();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector2 mv = new Vector2(movehorizontal, movevertical);
+        rb.AddForce(mv *speed);
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PickUp"))
+        {
+            collision.gameObject.SetActive(false);
+            c += 1;
+            setCount();
+        }
+    }
+
+    void setCount()
+    {
+        ctext.text = "Count :" + c.ToString();
+        if (c>=5)
+        {
+            wtext.text = "You Win!";
+        }
+    }
+}
